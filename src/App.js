2ed1d681b53237f5edd70535c13db226add5f77e
chamal_auxenta <Chamal.Gunasinghe@dialog.lk>
2021-08-21 12:34:42 +0530

    Initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { Button } from 'react-bootstrap';
+import { Entry } from './page/entry/EntryPage';
+
+function App() {
+  return (
+    <div className="App">
+      <Entry></Entry>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/entry/EntryPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import { Jumbotron } from 'react-bootstrap';
+
+import './entry.css';
+
+
+export const Entry = () => {
+
+    return (
+        <div className="entry-page bg-info">
+            <Jumbotron>entry page</Jumbotron>
+        </div>
+    )
+}
